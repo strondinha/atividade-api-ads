a9353abfe00372dd11a2f3a10f12f7c64d122c3f
Caio vieira vila nova <136968120+strondinha@users.noreply.github.com>
2026-05-21 20:07:30 -0300

    revisão

--- api.php
+++ api.php
@@ -4,14 +4,13 @@ header("Access-Control-Allow-Origin: *");
 
 $pagina = isset($_GET['page']) ? $_GET['page'] : 0;
 
-// Olha o link exato do professor aqui, usando a rota 'breeds' para buscar as raças!
 $url = "https://api.thecatapi.com/v1/breeds?limit=9&page=" . $pagina;
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-// Isso evita que o seu XAMPP bloqueie o link do professor
+// Isso evita que o seu XAMPP bloqueie o link
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
 
